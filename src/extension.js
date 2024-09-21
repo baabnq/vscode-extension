1722852ba8ccb4b5977d72819ecf8115c2842d42
S1mon <jaforsi.kozak@gmail.com>
2024-09-21 15:25:13 +0200

    remote runner working

---
+++ src/extension.js
@@ -0,0 +1,87 @@
+const vscode = require('vscode');
+const https = require('https');
+
+var outputPannel;
+
+function httpsPost({body, ...options}) {
+    return new Promise((resolve,reject) => {
+        const req = https.request({
+            method: 'POST',
+            ...options,
+        }, res => {
+            const chunks = [];
+            res.on('data', data => chunks.push(data))
+            res.on('end', () => {
+                let resBody = Buffer.concat(chunks);
+                resolve(resBody.toString());
+            })
+        })
+        req.on('error',reject);
+        req.write(body);
+        req.end();
+    })
+}
+
+
+
+async function run()
+{
+    const document = vscode.window.activeTextEditor?.document;
+    if (!document) return;
+
+    if (!document.isUntitled) document.save();
+    const text = document.getText();
+
+    outputPannel.show(true);
+    outputPannel.clear();
+    outputPannel.appendLine("[Running...]");
+
+    const res = httpsPost({
+        method: 'POST',
+        hostname: 'baabnq.eu.pythonanywhere.com',
+        path: '/run',
+        headers : {
+            'Content-Type': 'application/json;charset=UTF-8'
+        },
+        body: JSON.stringify({
+            source: text,
+        })
+    })
+
+    res.then((output) => {
+        outputPannel.clear();
+        outputPannel.append(output);
+    }, () => {
+        outputPannel.clear(),
+        outputPannel.appned("Error: Remote Runner not reachable");
+    });
+
+
+
+}
+
+
+
+function activate(ctx)
+{
+    outputPannel = vscode.window.createOutputChannel("Code");
+
+
+    const runCommand = vscode.commands.registerCommand(
+        'baabnq.run', 
+        run
+    );
+    ctx.subscriptions.push(runCommand);
+
+
+}
+
+
+
+
+function deactivate() {}
+
+module.exports = {
+    activate,
+    deactivate
+}
